f2b260e25eb133786d0017539b9724b413cc6188
zsga <github@gaoa.cc>
2024-05-07 21:36:03 +0800

    Inital project

---
+++ uno.config.ts
@@ -0,0 +1,42 @@
+// uno.config.ts
+import {
+  defineConfig,
+  presetIcons,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        height: '1.2em',
+        width: '1.2em',
+        'vertical-align': 'text-bottom',
+      },
+    }),
+    presetUno(),
+    presetWebFonts({
+      fonts: {
+        sans: 'Inter:400,600,800',
+        mono: 'DM Mono:400,600',
+        condensed: 'Roboto Condensed',
+        wisper: 'Bad Script',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives({
+      applyVariable: ['--at-apply', '--uno-apply', '--uno'],
+    }),
+  ],
+  shortcuts: [
+    {
+      'bg-base': 'bg-white dark:bg-black',
+      'color-base': 'text-black dark:text-white',
+      'border-base': 'border-[#8884]',
+    },
+  ],
+})
