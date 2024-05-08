4365114ec76756665b596c4dcaf6efb86299c5cf
zsga <github@gaoa.cc>
2024-05-08 22:00:54 +0800

    add mdx and shiki support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
+import {
+  transformerNotationDiff,
+  transformerNotationFocus,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from '@shikijs/transformers'
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash'
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
+
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,5 +16,25 @@ export default defineConfig({
     UnoCSS({
       injectReset: true,
     }),
+    mdx(),
   ],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        dark: 'github-dark',
+        light: 'github-light',
+      },
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      transformers: [
+        transformerTwoslash({
+          renderer: rendererRich(),
+        }),
+        transformerNotationDiff(),
+        transformerNotationFocus(),
+        transformerNotationHighlight(),
+        transformerNotationWordHighlight(),
+      ],
+    },
+  },
 })
